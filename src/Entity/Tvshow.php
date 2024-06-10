708320155e57c4d320096df4ea675167a6a2f063
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-10 10:20:43 +0200

    Ajout des Accesseurs des attributs de la classe Tvshow.php.

--- src/Entity/Tvshow.php
+++ src/Entity/Tvshow.php
@@ -12,4 +12,35 @@ class Tvshow
     private string $homepage;
     private string $overview;
     private ?int $posterId ;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getOriginalName(): string
+    {
+        return $this->originalName;
+    }
+
+    public function getHomepage(): string
+    {
+        return $this->homepage;
+    }
+
+    public function getOverview(): string
+    {
+        return $this->overview;
+    }
+
+    public function getPosterId(): ?int
+    {
+        return $this->posterId;
+    }
+
 }
