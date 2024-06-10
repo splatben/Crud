84e94237c74c8f352e8f6412b577f28136c0c6bf
Benoit  Collot <benoit.collot@etudiant.univ-reims.fr>
2024-06-10 17:12:04 +0200

    Ajout methode findByGenreId

--- src/Entity/Collection/TvshowCollection.php
+++ src/Entity/Collection/TvshowCollection.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Entity\Collection;
 
 use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
 use Entity\Tvshow;
 use PDO;
 
@@ -20,5 +21,22 @@ SQL);
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_CLASS, Tvshow::class);
+    }
+
+    public static function findByGenreId(Int $id): array
+    {
+        $req = MyPdo::getInstance()->prepare(
+            <<<SQL
+        Select * 
+        From tvshow
+        Where id IN(Select tvShowId From tvshow_genre Where genreId = ?)
+        SQL
+        );
+        $req->execute([$id]);
+        if (empty($tv_show = $req->fetchAll(PDO::FETCH_CLASS, Tvshow::class))) {
+            throw new EntityNotFoundException();
+        } else {
+            return $tv_show;
+        }
     }
 }
