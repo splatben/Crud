1d44d90f53a3bd2116b4d542ca70a762c665e667
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-10 14:14:25 +0200

    correction TvshowCollection

--- src/Entity/Collection/TvshowCollection.php
+++ src/Entity/Collection/TvshowCollection.php
@@ -5,18 +5,19 @@ declare(strict_types=1);
 namespace Entity\Collection;
 
 use Database\MyPdo;
+use Entity\Tvshow;
 use PDO;
-use src\Entity\Tvshow;
 
 class TvshowCollection
 {
     public static function findAll(): array
     {
-        $stmt = MyPdo::getInstance()->prepare(<<<SQL
+        $stmt = MyPdo::getInstance()->prepare(
+            <<<SQL
         SELECT *
-        FROM artist
-        ORDER BY name
-SQL);
+        FROM tvshow
+SQL
+        );
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_CLASS, Tvshow::class);
