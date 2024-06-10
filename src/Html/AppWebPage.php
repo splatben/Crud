2e3754cf8f7d16e6ed84354e349343d1f43953e8
Benoit  Collot <benoit.collot@etudiant.univ-reims.fr>
2024-06-10 13:34:22 +0200

    Ajout de la Methode to_Html

--- src/Html/AppWebPage.php
+++ src/Html/AppWebPage.php
@@ -18,4 +18,33 @@ class AppWebPage extends WebPage
         parent::__construct($title);
         parent::appendCssUrl("css/style1.css");
     }
+    public function toHtml(): string
+    {
+        return <<<HTML
+        <!doctype html>
+        <html lang = "fr">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title> {$this->getTitle()} </title>
+            {$this->getHead()}
+        </head>
+        <body>
+        <div class="header">
+            <h1>{$this->getTitle()}</h1>
+        </div>
+        <div class = "menu">
+            <button onclick="window.location.href = 'http://localhost:8000/index.php';"> Index </button>
+            {$this->menu}
+        </div>
+        <div class = "content" >
+            {$this->getBody()}
+        </div>
+        <div class = "footer">
+            <p> {$this->getLastModification()}</p>
+        </div>
+        </body>
+        </html>
+        HTML;
+    }
 }
