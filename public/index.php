1fdab4d9d413535935bb788ef6546393343b66e4
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-10 15:36:07 +0200

    Création de la page index.php

--- public/index.php
+++ public/index.php
@@ -1,1 +1,36 @@
 <?php
+
+declare(strict_types=1);
+
+
+use Entity\Collection\TvshowCollection;
+use Html\AppWebPage;
+use Entity\Tvshow;
+
+$webPage = new AppWebPage();
+$webPage->setTitle("Séries TV");
+
+$webPage->appendContent(<<<HTML
+<div class="list__show">
+
+HTML);
+
+$tvshows = TvshowCollection::findAll();
+foreach ($tvshows as $show) {
+    $source = "img/default.png";
+
+    $webPage->appendContent(<<<HTML
+    <div class="show">
+        <img src="$source" alt="poster">
+        <div class="show__info">
+            <a>{$show->getName()}</a>
+            <a>{$show->getOverview()}</a>
+        </div>
+    </div>
+
+
+HTML);
+}
+
+$webPage->appendContent('</div>');
+echo $webPage->toHtml();
