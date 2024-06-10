fc600cd8252bc66f950b061726726032752ecd8d
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-10 16:41:20 +0200

    Ajout de la page poster.php

--- public/poster.php
+++ public/poster.php
@@ -3,8 +3,25 @@
 declare(strict_types=1);
 
 
-try {
-    if (!empty($_GET['posterId']) && ctype_digit($_GET['posterId'])) {
+use Entity\Exception\EntityNotFoundException;
+use Entity\Poster;
+use Exception\ParameterException;
 
+try {
+    if (isset($_GET['posterId']) && !empty($_GET['posterId']) && ctype_digit($_GET['posterId'])) {
+        $posterId = (int) $_GET['posterId'];
+    } else {
+        throw new ParameterException();
     }
+
+    $poster = Poster::findById($posterId);
+    header('content-Type: image/jpeg');
+    echo $poster->getJpeg();
+
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
 }
