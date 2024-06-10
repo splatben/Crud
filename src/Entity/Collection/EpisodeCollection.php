338cf8c4102434d817ba012cbd2c57093b6125ef
Benoit  Collot <benoit.collot@etudiant.univ-reims.fr>
2024-06-10 16:21:30 +0200

    Creation class episodeCollection +MethodeFindAll

---
+++ src/Entity/Collection/EpisodeCollection.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Entity\Collection;
+
+use Database\MyPdo;
+use Entity\Episode;
+use Entity\Exception\EntityNotFoundException;
+use PDO;
+
+class EpisodeCollection
+{
+    public static function findBySeasonId(Int $id): array
+    {
+        $req = MyPdo::getInstance()->prepare(<<<SQL
+        Select * 
+        From episode
+        Where seasonId = ?
+        ORDER BY episodeNumber
+        SQL);
+        $req->execute([$id]);
+        if (empty($eps = $req->fetchAll(PDO::FETCH_CLASS, Episode::class))) {
+            throw new EntityNotFoundException();
+        } else {
+            return $eps;
+        }
+    }
+}
