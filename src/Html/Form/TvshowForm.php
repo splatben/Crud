311540b77755c1d0efc03f7a75f3701d98128bde
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-11 15:36:45 +0200

    Ajout de la méthode getHtmlForm() dans la classe TvshowForm.php

--- src/Html/Form/TvshowForm.php
+++ src/Html/Form/TvshowForm.php
@@ -5,11 +5,13 @@ declare(strict_types=1);
 namespace Html\Form;
 
 use Entity\Tvshow;
+use Html\StringEscaper;
 
 class TvshowForm
 {
     private ?Tvshow $show;
 
+    use StringEscaper;
     /**
      * @param Tvshow|null $show
      */
@@ -21,6 +23,29 @@ class TvshowForm
     public  function getShow(): ?Tvshow
     {
         return $this->show;
+    }
+
+    public function getHtmlForm(string $action): string
+    {
+        return <<<HTML
+<form method="post" action="$action">
+    <input name="id" type="hidden" value="{$this->show?->getId()}">
+    <label> Nom
+        <input type="text" name="name" required value="{$this->escapeString($this->show?->getName())}">
+    </label>
+    <label> Nom de base
+        <input type="text" name="originalName" required value="{$this->escapeString($this->show?->getOriginalName())}">
+    </label> 
+    <label> Page d'accueil
+        <input type="text" name="homepage" required value="{$this->escapeString($this->show?->getHomepage())}">
+    </label> 
+    <label> Description
+        <input type="text" name="overview" required value="{$this->escapeString($this->show?->getOverview())}">
+    </label>
+    <button type="submit">Enregister</button>
+</form>
+HTML;
+
     }
 
 
