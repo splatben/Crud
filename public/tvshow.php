90530acdbdc97ab1f100e7cc052b0152b269df0c
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-11 09:58:54 +0200

    Ajout des balises try et catch pour envoyer les codes d'erreurs

--- public/tvshow.php
+++ public/tvshow.php
@@ -5,35 +5,34 @@ declare(strict_types=1);
 use Entity\Exception\EntityNotFoundException;
 use Entity\Tvshow;
 use Entity\Season;
+use Exception\ParameterException;
 use Html\AppWebPage;
 
-$tvshowId = null;
-if (isset($_GET['tvshowId']) && !empty($_GET['tvshowId']) && ctype_digit($_GET['tvshowId'])) {
-    $tvshowId = (int) $_GET['tvshowId'];
-} else {
-    header('location: /');
-    exit;
-}
-
 try {
+    $tvshowId = null;
+    if (isset($_GET['tvshowId']) && !empty($_GET['tvshowId']) && ctype_digit($_GET['tvshowId'])) {
+        $tvshowId = (int)$_GET['tvshowId'];
+    } else {
+        header('location: /');
+        exit;
+    }
+
+
     $show = Tvshow::findById($tvshowId);
-} catch (EntityNotFoundException) {
-    http_response_code(404);
-    exit;
-}
+    $webPage = new AppWebPage;
 
-$title = "Séries TV : {$show->getName()}";
-
-$webPage = new AppWebPage($title);
-$webPage->appendCssUrl("style/tvshow.css");
+    $title = "Séries TV : {$show->getName()}";
+    $webPage->setTitle($title);
+    $webPage = new AppWebPage($title);
+    $webPage->appendCssUrl("style/tvshow.css");
 
 
-$sourcePosterShow = "img/default.png";
-if ($show->getPosterId() !== null) {
-    $sourcePosterShow = "poster.php?posterId={$show->getPosterId()}";
-}
+    $sourcePosterShow = "img/default.png";
+    if ($show->getPosterId() !== null) {
+        $sourcePosterShow = "poster.php?posterId={$show->getPosterId()}";
+    }
 
-$webPage->appendContent(<<<HTML
+    $webPage->appendContent(<<<HTML
     <div class="show">
         <img src="$sourcePosterShow" alt="Poster show">
         <div class="show__info">
@@ -44,25 +43,35 @@ $webPage->appendContent(<<<HTML
     </div>
     <div class="list__season">
 
-HTML);
+HTML
+    );
 
-$seasons = $show->getSeasons();
+    $seasons = $show->getSeasons();
 
-foreach ($seasons as $season) {
-    $sourcePosterSeason = "img/default.png";
-    if ($season->getPosterId() !== null) {
-        $sourcePosterSeason = "poster.php?posterId={$season->getPosterId()}";
-    }
+    foreach ($seasons as $season) {
+        $sourcePosterSeason = "img/default.png";
+        if ($season->getPosterId() !== null) {
+            $sourcePosterSeason = "poster.php?posterId={$season->getPosterId()}";
+        }
 
-    $webPage->appendContent(<<<HTML
+        $webPage->appendContent(<<<HTML
         <div class="season">
             <img src="$sourcePosterSeason" alt="Poster season">
             <a class="season__title">{$season->getName()}</a>
         </div>
 
-HTML);
+HTML
+        );
+    }
+    $webPage->appendContent("</div>");
+
+
+    echo $webPage->toHtml();
+
+} catch (ParameterException $e){
+    http_response_code(400);
+} catch (EntityNotFoundException $e) {
+    http_response_code(404);
+} catch (Exception $e) {
+    http_response_code(500);
 }
-$webPage->appendContent("</div>");
-
-
-echo $webPage->toHtml();
