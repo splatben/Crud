dd9f9918fddbcb8ac00b7fd8b88b8e28d5750c19
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-11 15:20:33 +0200

    Ajout du constructeur et de l'accesseur de la classe TvshowForm.php

--- src/Html/Form/TvshowForm.php
+++ src/Html/Form/TvshowForm.php
@@ -8,5 +8,21 @@ use Entity\Tvshow;
 
 class TvshowForm
 {
-    private Tvshow $show;
+    private ?Tvshow $show;
+
+    /**
+     * @param Tvshow|null $show
+     */
+    public function __construct(?Tvshow $show = null)
+    {
+        $this->show = $show;
+    }
+
+    public  function getShow(): ?Tvshow
+    {
+        return $this->show;
+    }
+
+
+
 }
