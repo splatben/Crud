32f8a3b232de3821e379a43901a5d3209816fc28
Raphael Gomes <raphael.gomes1@etudiant.univ-reims.fr>
2024-06-11 16:12:56 +0200

    Création fichier tvshow-form.php

---
+++ public/admin/tvshow-form.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Entity\Exception\EntityNotFoundException;
+use Entity\Tvshow;
+use Exception\ParameterException;
+use Html\Form\TvshowForm;
+
+try {
+    $show = null;
+    if (!empty($_GET['tvshowId']) && ctype_digit($_GET['tvshowId'])) {
+        $show = Tvshow::findById((int) $_GET['tvshowId']);
+    } else {
+        throw new ParameterException();
+    }
+
+    $tvshowForm = new TvshowForm($show);
+    echo $tvshowForm->getHtmlForm("artist-save.php");
+
+} catch (ParameterException) {
+    http_response_code(400);
+} catch (EntityNotFoundException) {
+    http_response_code(404);
+} catch (Exception) {
+    http_response_code(500);
+}
